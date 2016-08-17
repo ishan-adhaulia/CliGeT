a5a18e5464c014dafb91413fc11e44241201fd52
malkomich <malkomich@gmail.com>
2016-08-17 19:05:42 +0200

    Widget init config

---
+++ app/src/main/java/com/malkomich/cliget/CliGetWidgetProvider.java
@@ -0,0 +1,16 @@
+package com.malkomich.cliget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by malkomich on 17/08/16.
+ */
+public class CliGetWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        
+    }
+}
